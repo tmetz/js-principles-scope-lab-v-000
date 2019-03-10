d6362341c6073781f5a5c0f56f1deec6bc91fe7d
Tammy Metz <tammy@tammymetz.com>
2019-03-10 00:18:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = "bob"; // global scope
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "Bob";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Tammy";
+}
