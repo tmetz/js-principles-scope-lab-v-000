6289a4daaa42c9a2427b7accb83bb33141a1882f
Tammy Metz <tammy@tammymetz.com>
2019-03-10 00:17:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName; // global scope
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "me";
+}
+
+const leastFavoriteCustomer = "Bob";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Tammy";
+}
